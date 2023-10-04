63d0aed47b7cf22af391309412341a730640a7ed
dikadzikrii <92675536+dikadzikrii@users.noreply.github.com>
2023-10-04 14:18:38 +0700

    Add files via upload

---
+++ tantanganPertama.dart
@@ -0,0 +1,12 @@
+void main() {
+  List<int> datalistku = [4, 7, 8, 2, 6, 3, 6, 78];
+  
+  // Mengurutkan list secara ascending (dari terkecil ke terbesar)
+  datalistku.sort();
+  
+  // Menambahkan angka 109 ke dalam list
+  datalistku.add(109);
+
+  // Cetak list yang sudah diurutkan dan ditambahkan angka 109
+  print(datalistku);
+}
